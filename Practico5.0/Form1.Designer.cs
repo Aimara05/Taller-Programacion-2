0f8307b7bbd0e1da184cafc9488841893a344cdc
Aimara Fernandez <ayelenfer879@gmail.com>
2023-08-31 00:12:22 -0300

    resolucion del practico n° 5

---
+++ Practico5.0/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Practico5._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FechaN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sexo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Eliminar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Saldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Foto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ruta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.LFechadeN = new System.Windows.Forms.Label();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.LSexo = new System.Windows.Forms.Label();
+            this.LSaldo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RBMujer = new System.Windows.Forms.RadioButton();
+            this.RBHombre = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.TFoto = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlDark;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Apellido,
+            this.Nombre,
+            this.FechaN,
+            this.Sexo,
+            this.Eliminar,
+            this.Saldo,
+            this.Foto,
+            this.Ruta});
+            this.dataGridView1.Location = new System.Drawing.Point(27, 337);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(850, 125);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            // 
+            // Apellido
+            // 
+            this.Apellido.Frozen = true;
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.Name = "Apellido";
+            this.Apellido.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            this.Nombre.Frozen = true;
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // FechaN
+            // 
+            this.FechaN.HeaderText = "Fecha Nacimiento";
+            this.FechaN.Name = "FechaN";
+            // 
+            // Sexo
+            // 
+            this.Sexo.HeaderText = "Sexo";
+            this.Sexo.Name = "Sexo";
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.HeaderText = "Eliminar";
+            this.Eliminar.Name = "Eliminar";
+            // 
+            // Saldo
+            // 
+            this.Saldo.HeaderText = "Saldo";
+            this.Saldo.Name = "Saldo";
+            // 
+            // Foto
+            // 
+            this.Foto.HeaderText = "Foto";
+            this.Foto.Name = "Foto";
+            // 
+            // Ruta
+            // 
+            this.Ruta.HeaderText = "Ruta";
+            this.Ruta.Name = "Ruta";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.TFoto);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.RBHombre);
+            this.panel2.Controls.Add(this.RBMujer);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.LSaldo);
+            this.panel2.Controls.Add(this.LSexo);
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.LApellido);
+            this.panel2.Controls.Add(this.LFechadeN);
+            this.panel2.Controls.Add(this.LNombre);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel2.Location = new System.Drawing.Point(68, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(425, 318);
+            this.panel2.TabIndex = 4;
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(21, 24);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(73, 25);
+            this.LNombre.TabIndex = 1;
+            this.LNombre.Text = "Nombre:";
+            // 
+            // LFechadeN
+            // 
+            this.LFechadeN.AutoSize = true;
+            this.LFechadeN.Location = new System.Drawing.Point(21, 115);
+            this.LFechadeN.Name = "LFechadeN";
+            this.LFechadeN.Size = new System.Drawing.Size(159, 25);
+            this.LFechadeN.TabIndex = 3;
+            this.LFechadeN.Text = "Fecha de Nacimiento:";
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(21, 65);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(76, 25);
+            this.LApellido.TabIndex = 2;
+            this.LApellido.Text = "Apellido:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 33);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(222, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 33);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(222, 111);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(107, 26);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // LSexo
+            // 
+            this.LSexo.AutoSize = true;
+            this.LSexo.Location = new System.Drawing.Point(21, 153);
+            this.LSexo.Name = "LSexo";
+            this.LSexo.Size = new System.Drawing.Size(52, 25);
+            this.LSexo.TabIndex = 7;
+            this.LSexo.Text = "Sexo";
+            // 
+            // LSaldo
+            // 
+            this.LSaldo.AutoSize = true;
+            this.LSaldo.Location = new System.Drawing.Point(21, 189);
+            this.LSaldo.Name = "LSaldo";
+            this.LSaldo.Size = new System.Drawing.Size(57, 25);
+            this.LSaldo.TabIndex = 8;
+            this.LSaldo.Text = "Saldo";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button1.Image = global::Practico5._0.Properties.Resources.disco_flexible;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(213, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 44);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Agregar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RBMujer
+            // 
+            this.RBMujer.AutoSize = true;
+            this.RBMujer.Location = new System.Drawing.Point(222, 149);
+            this.RBMujer.Name = "RBMujer";
+            this.RBMujer.Size = new System.Drawing.Size(70, 29);
+            this.RBMujer.TabIndex = 10;
+            this.RBMujer.TabStop = true;
+            this.RBMujer.Text = "Mujer";
+            this.RBMujer.UseVisualStyleBackColor = true;
+            // 
+            // RBHombre
+            // 
+            this.RBHombre.AutoSize = true;
+            this.RBHombre.Location = new System.Drawing.Point(323, 151);
+            this.RBHombre.Name = "RBHombre";
+            this.RBHombre.Size = new System.Drawing.Size(89, 29);
+            this.RBHombre.TabIndex = 11;
+            this.RBHombre.TabStop = true;
+            this.RBHombre.Text = "Hombre";
+            this.RBHombre.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(222, 189);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(190, 33);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // TFoto
+            // 
+            this.TFoto.Location = new System.Drawing.Point(222, 232);
+            this.TFoto.Name = "TFoto";
+            this.TFoto.Size = new System.Drawing.Size(190, 33);
+            this.TFoto.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(26, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Foto";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.BackgroundImage = global::Practico5._0.Properties.Resources._456212;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Location = new System.Drawing.Point(499, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(232, 318);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.BackgroundImage = global::Practico5._0.Properties.Resources.cc7480151c46f38997f9d3523ee2fe2e;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(12, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(765, 322);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 484);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Formulario con DataGrid";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FechaN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sexo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Eliminar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Saldo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Foto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ruta;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox TFoto;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton RBHombre;
+        private System.Windows.Forms.RadioButton RBMujer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LSaldo;
+        private System.Windows.Forms.Label LSexo;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LFechadeN;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Practico5.0/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practico5._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "C:";
+            openFileDialog1.Filter = "Archivos Imagenes|*.jpg;*.png|Todos los archivos|*.*";
+            openFileDialog1.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(openFileDialog1.FileName))
+            {
+                pictureBox2.ImageLocation = openFileDialog1.FileName;
+
+                //funciones para ajustar el tamaño de la imagen en el picturebox.
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                TFoto.Text = Path.GetFileName(openFileDialog1.FileName);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+
+                string input = textBox1.Text;
+                string formatoNombre = char.ToUpper(input[0]) + input.Substring(1).ToLower();
+                textBox1.Text = formatoNombre;
+
+
+                // coloca el cursor al final del texto.
+                textBox1.SelectionStart = formatoNombre.Length;
+                textBox1.SelectionLength = 0;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty (textBox2.Text))
+            {
+                string input = textBox2.Text;
+                string formatoApellido = char.ToUpper(input[0]) + input.Substring(1).ToLower();
+                textBox2.Text = formatoApellido;
+
+
+                // colocar el cursor al final del texto.
+                textBox2.SelectionStart = formatoApellido.Length;
+                textBox2.SelectionLength = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Validación de los radio buttons
+            if (!RBMujer.Checked && !RBHombre.Checked)
+            {
+                MessageBox.Show("Debe seleccionar un género", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Salir del método si no se ha seleccionado un género
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text) || string.IsNullOrWhiteSpace(textBox3.Text) || string.IsNullOrWhiteSpace(TFoto.Text))
+            {
+                MessageBox.Show("Debe completar todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Salir del método si algún campo está vacío
+            }
+
+            // se agrega una nueva fila
+            int n = dataGridView1.Rows.Add();
+
+                // se coloca la información
+                dataGridView1.Rows[n].Cells[0].Value = textBox2.Text;
+                dataGridView1.Rows[n].Cells[1].Value = textBox1.Text;
+                dataGridView1.Rows[n].Cells[2].Value = dateTimePicker1.Text;
+
+                if (RBMujer.Checked)
+                {
+                    dataGridView1.Rows[n].Cells[3].Value = "Mujer";
+                }
+                else
+                {
+                    dataGridView1.Rows[n].Cells[3].Value = "Hombre";
+                }
+
+                //creacion y asignacion del boton eliminar.
+
+                DataGridViewButtonCell deleteButtonCell = new DataGridViewButtonCell();
+                deleteButtonCell.Value = "Eliminar";
+                dataGridView1.Rows[n].Cells[4] = deleteButtonCell;
+
+                dataGridView1.Rows[n].Cells[5].Value = textBox3.Text;
+
+                // Redimensionar la imagen a una versión en miniatura
+                if (pictureBox2.Image != null)
+                {
+                    int thumbnailWidth = 80; // Ancho de la miniatura
+                    int thumbnailHeight = 80; // Altura de la miniatura
+
+                    // Crear una versión en miniatura de la imagen
+                    Image thumbnail = pictureBox2.Image.GetThumbnailImage(thumbnailWidth, thumbnailHeight, null, IntPtr.Zero);
+
+                    // Crear una instancia de DataGridViewImageCell
+                    DataGridViewImageCell imageCell = new DataGridViewImageCell();
+
+                    // Asignar la imagen en miniatura a la celda de imagen
+                    imageCell.Value = thumbnail;
+
+                    // Asignar la celda de imagen a la celda correspondiente en la fila
+                    dataGridView1.Rows[n].Cells[6] = imageCell;
+                }
+
+                dataGridView1.Rows[n].Cells[7].Value = TFoto.Text;
+
+                //codigo para cambiar el color en caso de que el saldo sea menor a 50, punto 8 del practico 5.
+                decimal saldo;
+                if (decimal.TryParse(textBox3.Text, out saldo))
+                {
+                    if (saldo < 50)
+                    {
+                        dataGridView1.Rows[n].DefaultCellStyle.BackColor = Color.Red;
+                    }
+                }
+
+                //limpiamos los textbox
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                dateTimePicker1.ResetText();
+                TFoto.Clear();
+
+                string rutaImagen = "C:\\Users\\Usuario\\source\\repos\\Practico5.0\\Resources\\456212.png";
+                pictureBox2.Image = Image.FromFile(rutaImagen);
+
+                dataGridView1.AutoResizeColumns();
+                dataGridView1.AutoResizeRows();
+
+
+            
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == 4 && e.RowIndex >= 0) 
+            {
+                if(MessageBox.Show("¿Estás seguro que deseas eliminar este registro?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    dataGridView1.Rows.RemoveAt(e.RowIndex);
+                }
+            }
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if(e.RowIndex >= 0 && (e.ColumnIndex == 0 || e.ColumnIndex == 1)) 
+            {
+                e.CellStyle.Font = new Font(e.CellStyle.Font, FontStyle.Bold);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solamente se pueden letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solamente se pueden letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+       
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solamente se pueden ingresar números", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+    }
+
+}
